9e28868156ff2ee9684645d01b93b185cfa09b28
Nat-Hunt <nathan.huntlds@gmail.com>
2022-09-14 12:46:37 -0500

    W01 Assignment: Course Assignment Portal Page

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+const currentDate = new Date();
+const copyright = "&copy ";
+const name = " | Nathan T. Hunt | Alabama";
+document.querySelector('#year').innerHTML = copyright.concat(currentDate.getFullYear(), name);
+const lastUpdate = "Last Updated: ";
+document.querySelector('#last-update').textContent = lastUpdate.concat(document.lastModified);
