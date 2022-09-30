d851abb30dea3865725e848ec1e7ac2547626379
Nat-Hunt <nathan.huntlds@gmail.com>
2022-09-30 15:02:25 -0500

    lesson3, Site Plan

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,11 @@
 const currentDate = new Date();
 const copyright = "&copy ";
-const name = " | Nathan T. Hunt | Alabama";
-document.querySelector('#year').innerHTML = copyright.concat(currentDate.getFullYear(), name);
+const author = " .:|:. Nathan T. Hunt .:|:. Alabama";
+document.querySelector("#year").innerHTML = copyright.concat(
+  currentDate.getFullYear(),
+  author
+);
 const lastUpdate = "Last Updated: ";
-document.querySelector('#last-update').textContent = lastUpdate.concat(document.lastModified);
+document.querySelector("#last-update").textContent = lastUpdate.concat(
+  document.lastModified
+);
