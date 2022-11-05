d288a4448301d89358e882f13f7123da88b3b0fd
Nat-Hunt <nathan.huntlds@gmail.com>
2022-11-05 16:31:14 -0500

    replace test image for main site image

--- chamber/js/main.js
+++ chamber/js/main.js
@@ -6,3 +6,22 @@ const banner = document.getElementById("banner");
 if (today.getDay() == 1 || today.getDay() == 2) {
   banner.classList.toggle("itsMondayOrTuesday");
 }
+
+// function displayText() {
+//   const w = document.documentElement.clientWidth;
+
+//   if (w < 560) {
+//     document.querySelector("#welcome h3").innerHTML =
+//       "The Treasure on the Tallapoosa <br><br>SMALL IMAGE";
+//   } else if (w >= 560 && w <= 1379) {
+//     document.querySelector("#welcome h3").innerHTML =
+//       "The Treasure on the Tallapoosa <br><br>MEDIUM IMAGE";
+//   } else if (w >= 1380) {
+//     document.querySelector("#welcome h3").innerHTML =
+//       "The Treasure on the Tallapoosa <br><br>LARGE IMAGE";
+//   }
+// }
+
+// const x = window;
+// x.addEventListener("resize", displayText);
+// displayText();
--- chamber/js/windchill.js
+++ chamber/js/windchill.js
@@ -12,22 +12,3 @@ if (t <= 50 && s > 3.0) {
 } else {
   document.getElementById("windChill").innerHTML = "N/A";
 }
-
-function displayText() {
-  const w = document.documentElement.clientWidth;
-
-  if (w < 560) {
-    document.querySelector("#welcome h3").innerHTML =
-      "The Treasure on the Tallapoosa <br><br>SMALL IMAGE";
-  } else if (w >= 560 && w <= 1379) {
-    document.querySelector("#welcome h3").innerHTML =
-      "The Treasure on the Tallapoosa <br><br>MEDIUM IMAGE";
-  } else if (w >= 1380) {
-    document.querySelector("#welcome h3").innerHTML =
-      "The Treasure on the Tallapoosa <br><br>LARGE IMAGE";
-  }
-}
-
-const x = window;
-x.addEventListener("resize", displayText);
-displayText();
